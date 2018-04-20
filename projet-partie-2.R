d199e71148b2d0307b0ee53a206956118304cc32
tmolcard <molcard.t@gmail.com>
2018-04-20 16:58:49 +0200

    function kmeans-addaptative

--- projet-partie-2.R
+++ projet-partie-2.R
@@ -8,40 +8,40 @@ kmeans.addaptative <- function(X, k, volk = rep(1, k), niter = 100, ness = 1, ep
   
   U.k <- X[sample(n, k), ]                        # center of groups
   
-  V.k <- array(diag(p), c(p, p, k)) * volk^(-1/p) # inv. mat. cov.
+  V.k <- array(diag(p), c(p, p, k)) * volk^(-1/p) # cov. mat.
   
-  grp <- array(NA, n)                             # groupes de chaque indiv. par indices
-  dist.X <- matrix(NA, n, k)                      # matrice des distances
+  grp <- array(NA, n)                             # groups
+  dist.X <- matrix(NA, n, k)                      # dist. mat.
   
-  # iterations
   for (i in 1:k) {
-    dist.X[,i] <- distXY(X, U.k[i,], V.k[,,i])
+    dist.X[,i] <- distXY(X, U.k[i,], solve(V.k[,,i]))
   }
-  
   grp <- apply(dist.X, 1, which.min)
   
-#  U.k <- apply(t(1:k), 1, function(i) colMeans(X[grp==i,]))
-#  V.k <- apply(t(1:k), 1, function(i) sum((X[grp==i,] - U.k[i,]) %*% t(X[grp==i,] - U.k[i,])))
-  
-  for (i in 1:k) {
-    grp.i <- grp == i
-    U.k[i,] <- colMeans(X[grp.i,])
-    #V.k[,,i] <- print((X[grp.i,] - U.k[i,]) %*% t(X[grp.i,] - U.k[i,]))
-  #print((X[grp.i,] - U.k[i,]) %*% t(X[grp.i,] - U.k[i,]))
-
-  print(apply(X[grp.i,], 1, function(x) (x - U.k[i,]) %*% t(x - U.k[i,])))
+  # iterations
   
   
-        
+  for (iter in 1:niter) {
+    U.kp <- U.k
+    for (i in 1:k) {
+      grp.i <- grp == i
+      U.k[i,] <- colMeans(X[grp.i,])                        # update centers
+      V.k[,,i] <- cov(X[grp.i,] - U.k[i,])                  # update cov. mat.
+      V.k[,,i] <- V.k[,,i] * (volk[i]*det(V.k[,,i]))^(-1/p) # normalizing
+      
+      dist.X[,i] <- distXY(X, U.k[i,], solve(V.k[,,i])) # compute new Mahalanobis dist.
+    }
+    
+    grp <- apply(dist.X, 1, which.min) 
+    
+    if (sum((U.k-U.kp)^2) < eps) {
+      break
+    }
   }
-  #print(X[1,])
-  #print(U.k[1,])
-  #print((X[1,] - U.k[1,]) %*% t(X[1,] - U.k[1,]))
   
-  #print((X[1,]) %*% t(X[1,]))
+  result <- list(groups = grp, centers = U.k, nrml.cov = V.k, nb.iter = iter)
   
-  
-  return(grp)
+  return(result)
 
 }
 
